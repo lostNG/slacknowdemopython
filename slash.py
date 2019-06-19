9fdd25a427118660549f78c436568d66fa3f3dd4
Erik Kalkoken <erik.kalkoken@gmail.com>
2019-06-19 17:11:43 +0200

    extended user message

--- slash.py
+++ slash.py
@@ -7,7 +7,7 @@ app = Flask(__name__) #create the Flask app
 @app.route('/', methods=['POST'])
 def slash_response():        
     user_id = request.form.get('user_id')
-    response = '{"text": "Hi, <@' + user_id + '>"}'
+    response = '{"text": "Hi there, <@' + user_id + '>"}'
     return response, 200, {'content-type': 'application/json'}
 
 if __name__ == '__main__':
