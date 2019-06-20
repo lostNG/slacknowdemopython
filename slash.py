d490c509da48903222878583b104fbdbe5b99bff
Stefano Loi <stefano.loi@nielsen.com>
2019-06-20 08:32:31 +0100

    Update slash.py

--- slash.py
+++ slash.py
@@ -2,12 +2,13 @@
 
 from flask import Flask, request, make_response, Response, jsonify
 
+
 app = Flask(__name__) #create the Flask app
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Visit the <a href='./about'>about</a> page or view the <a href='%s'>source code</a>.</p>" % (css, source), mimetype='text/html')
+    return Response("<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p>", mimetype='text/html')
 
 
 if __name__ == '__main__':
