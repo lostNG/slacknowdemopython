2ed3cd0eddc13d7d7ce8b6795c4d470683ea53bc
Stefano Loi <stefano.loi@nielsen.com>
2019-06-20 09:19:08 +0100

    slash update

--- slash.py
+++ slash.py
@@ -1,16 +1,14 @@
 #app.py
 
-from flask import Flask, request, make_response, Response, jsonify
-
-
+from flask import Flask, request #import main Flask class and request object
 
 app = Flask(__name__) #create the Flask app
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p>", mimetype='text/html')
-
+@app.route('/', methods=['POST'])
+def slash_response():
+    user_id = request.form.get('user_id')
+    response = '{"text": "Hi there, <@' + user_id + '>"}'
+    return response, 200, {'content-type': 'application/json'}
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000) #run app in debug mode on port 5000
