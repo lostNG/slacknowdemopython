37935b90743ffc098027560353de5c37cd12f71e
Stefano Loi <stefano.loi@nielsen.com>
2019-06-20 09:25:06 +0100

    Update slash.py

--- slash.py
+++ slash.py
@@ -4,6 +4,9 @@ from flask import Flask, request #import main Flask class and request object
 
 app = Flask(__name__) #create the Flask app
 
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+
+
 @app.route('/', methods=['POST'])
 def slash_response():
     user_id = request.form.get('user_id')
